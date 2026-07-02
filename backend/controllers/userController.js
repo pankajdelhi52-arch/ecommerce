0b9747f4cdd0a25487cd94a30db023f0c596fee7
Pankaj Yadav <Pankajdelhi52@gmail.com>
2026-07-02 19:14:13 +0530

    login process complete in backend

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,6 @@
 import User from "../models/user.js";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 export const signupUser = async (req, res) => {
     try {
@@ -28,3 +29,38 @@ export const signupUser = async (req, res) => {
         res.status(500).json({ message: "Server error", error: error.message });
     }
 }
+    export const login = async (req, res) => {
+          try {
+        const { email, password }=req.body;
+        const user=await User.findOne({email});
+        if(!user){
+            return res.status(404).json({message:"User not found"});
+        }
+        const isPasswordValid=await bcrypt.compare(password,user.password);
+        if(!isPasswordValid){
+            return res.status(400).json({message:"invalid password"});
+
+        }
+        else{
+            const token=jwt.sign(
+                {id:user._id},
+                process.env.JWT_SECRET,
+                {expiresIn:"1h"}
+            );
+            res.status(200).json(
+                {
+                    message:"Login successful",
+                token,
+                user,
+            },
+            );
+        }
+    }catch (error) {
+    console.error(error);
+
+    res.status(500).json({
+        message: "Internal server error",
+        error: error.message
+    });
+}
+    }
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,8 +1,9 @@
 import express from 'express';
-import {signupUser} from '../controllers/userController.js';
+import {login , signupUser} from '../controllers/userController.js';
 
 const router = express.Router();
 
 router.post('/signup', signupUser);
+router.post('/login', login);
 
 export default router;
